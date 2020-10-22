73e60cded1e834ecc42ce818e81f4e8d22febd04
mjbrusso <mjbrusso@gmail.com>
2020-10-21 21:36:26 -0300

    Create bg, tux and oil

--- 10-21/game.cpp
+++ 10-21/game.cpp
@@ -2,14 +2,32 @@
 
 int main() {
     const int windowWidth = 800, windowHeight = 400;
+    const float step_time = 0.1, groundSpeed = 1.2;
 
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Meu game");
     window.setFramerateLimit(60);
 
-    sf::CircleShape shape(20);
-    shape.setFillColor(sf::Color::Green);
-    float x=0;
+    sf::Clock clock;
 
+    sf::Texture bgTexture;
+    bgTexture.loadFromFile("assets/background.png");
+    bgTexture.setRepeated(true);
+    sf::IntRect bgRect(0, 0, windowWidth, windowHeight);
+    sf::Sprite bgSprite(bgTexture);
+
+    sf::IntRect grRect(0, 375, windowWidth, 25);
+    sf::Sprite grSprite(bgTexture);
+
+    sf::Texture tuxTexture;
+    tuxTexture.loadFromFile("assets/supertux.png");
+    sf::IntRect tuxRect(12, 786, 42, 67);
+    sf::Sprite tuxSprite(tuxTexture);
+
+    sf::Texture oilTexture;
+    oilTexture.loadFromFile("assets/oil.png");
+    sf::Sprite oilSprite(oilTexture);
+
+    float tuxY = windowHeight - 90, oilX = windowWidth;
     // game loop
     while (window.isOpen()) {
         // Tratamento dos eventos
@@ -23,12 +41,34 @@ int main() {
             }
         }
         // Atualização do estado do jogo
-        x++;
-        shape.setPosition(x, 200);
+        bgRect.left++;
+        bgSprite.setTextureRect(bgRect);
+        
+        grSprite.setPosition(0, 375);
+        grRect.left -= groundSpeed;
+        grSprite.setTextureRect(grRect);
+
+        if(clock.getElapsedTime().asSeconds() >= step_time){
+            tuxRect.left += 65;
+            if(tuxRect.left >= 470) tuxRect.left = 12;
+            clock.restart();
+        }
+        
+        tuxSprite.setTextureRect(tuxRect);
+        tuxSprite.setPosition(20, tuxY);
+
+        oilX -= groundSpeed;
+        if(oilX <= -65) oilX = windowWidth;
+        oilSprite.setPosition(oilX, windowHeight-27);
+
+    
 
         // Desenha o frame
         window.clear(sf::Color::White);
-        window.draw(shape);
+        window.draw(bgSprite);
+        window.draw(grSprite);
+        window.draw(tuxSprite);
+        window.draw(oilSprite);
         window.display();
     }
 
